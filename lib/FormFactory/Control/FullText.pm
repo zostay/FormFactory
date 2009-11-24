122ed3b29ea3e5eae5f7429893a3f62c8aa914f7
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-11-23 21:46:08 -0600

    Adding POD to FormFactory::Control::FullText, Password

--- lib/FormFactory/Control/FullText.pm
+++ lib/FormFactory/Control/FullText.pm
@@ -7,11 +7,44 @@ with qw(
     FormFactory::Control::Role::ScalarValue
 );
 
+=head1 NAME
+
+FormFactory::Control::FullText - The full_text control
+
+=head1 SYNOPSIS
+
+  has_control message => (
+      control => 'full_text',
+      options => {
+          default_value => q{Lots of
+            text goes
+            in here.},
+      },
+  );
+
+=head1 DESCRIPTION
+
+This is a multi-line text control.
+
+=head1 ATTRIBUTES
+
+=head2 value
+
+The current value of the control.
+
+=cut
+
 has value => (
     is        => 'rw',
     isa       => 'Str',
     predicate => 'has_value',
 );
+
+=head2 default_value
+
+The default value of the control.
+
+=cut
 
 has default_value => (
     is        => 'rw',
@@ -19,9 +52,23 @@ has default_value => (
     predicate => 'has_default_value',
 );
 
+=head2 stashable_keys
+
+The L</value> is stashed.
+
+=cut
+
 has '+stashable_keys' => (
     default   => sub { [ qw( value ) ] },
 );
+
+=head1 METHODS
+
+=head2 current_value
+
+This use the L</value> if available. It falls back to L</default_value> otherwise. It returns an empty string if neither are set.
+
+=cut
 
 sub current_value {
     my $self = shift;
@@ -30,5 +77,18 @@ sub current_value {
          : $self->has_default_value ? $self->default_value
          :                            '';
 }
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp C<< <hanenkamp@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Qubling Software LLC.
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 1;
--- lib/FormFactory/Control/Password.pm
+++ lib/FormFactory/Control/Password.pm
@@ -7,11 +7,41 @@ with qw(
     FormFactory::Control::Role::ScalarValue
 );
 
+=head1 NAME
+
+FormFactory::Control::Password - the password control
+
+=head1 SYNOPSIS
+
+  has_control new_password => (
+      control => 'password',
+  );
+
+head1 DESCRIPTION
+
+This is a password control. It is similar to a text control, but does not stash anything and has no default value.
+
+=head1 ATTRIBUTES
+
+=head2 value
+
+This is the current value of the password control.
+
+=cut
+
 has value => (
     is        => 'rw',
     isa       => 'Str',
     predicate => 'has_value',
 );
+
+=head1 METHODS
+
+=head2 current_value
+
+This returns L</value> if set or an empty string.
+
+=cut
 
 sub current_value {
     my $self = shift;
@@ -19,5 +49,18 @@ sub current_value {
     return $self->has_value ? $self->value
          :                    '';
 }
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp C<< <hanenkamp@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Qubling Software LLC.
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 1;
