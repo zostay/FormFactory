0c47e6fc9b94a8c39103b4f0744dda115381a370
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-11-23 19:57:29 -0600

    Adding POD to FormFactory::Control::Choice

--- lib/FormFactory/Control/Choice.pm
+++ lib/FormFactory/Control/Choice.pm
@@ -1,10 +1,58 @@
 package FormFactory::Control::Choice;
 use Moose;
 
+=head1 NAME
+
+FormFactory::Control::Choice - Helper class for tracking choices
+
+=head1 SYNOPSIS
+
+  my $foo = FormFactory::Control::Choice->new('foo');
+  my $bar = FormFactory::Control::Choice->new('bar' => 'Bar');
+  my $baz = FormFactory::Control::Choice->new(
+      label => 'Baz',
+      value => 'baz',
+  );
+  my $qux = FormFactory::Control::Choice->new({
+      label => 'Qux',
+      value => 'qux',
+  });
+
+=head1 DESCRIPTION
+
+These objects represent a single choice for a list or popup box. Each choice has a label and a value. The constructor is flexible to allow the following uses:
+
+  my $choice = FormFactory::Control::Choice->new($value) # $label = $value
+  my $choice = FormFactory::Control::Choice->new($value => $label);
+  my $choice = FormFactory::Control::Choice->new(
+      label => $label,
+      value => $value,
+  );
+  my $choice = FormFactory::Control::Choice->new({
+      label => $label,
+      value => $value,
+  });
+
+If C<$value> and C<$label> are the same, all of those calls are identical.
+
+=head1 ATTRIBUTES
+
+=head2 label
+
+The label to give the choice.
+
+=cut
+
 has label => (
     is        => 'ro',
     isa       => 'Str',
 );
+
+=head2 value
+
+The value of the choice.
+
+=cut
 
 has value => (
     is        => 'ro',
@@ -34,5 +82,18 @@ sub BUILDARGS {
 
     return $class->SUPER::BUILDARGS(%args);
 }
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp C<< <hanenkamp@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Qubling Software LLC.
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 1;
