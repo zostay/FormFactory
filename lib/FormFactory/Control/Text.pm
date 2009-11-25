0071ba2908401e6c1d5e78bbaae06c5283d425d6
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-11-24 22:21:33 -0600

    Add POD to FormFactory::Control::Text

--- lib/FormFactory/Control/Text.pm
+++ lib/FormFactory/Control/Text.pm
@@ -7,11 +7,46 @@ with qw(
     FormFactory::Control::Role::ScalarValue
 );
 
+=head1 NAME
+
+FormFactory::Control::Text - A single line text field
+
+=head1 SYNOPSIS
+
+  has_control your_name => (
+      control => 'text',
+      options => {
+          label         => 'Your Real Name',
+          default_value => 'Thomas Anderson',
+      },
+  );
+
+=head1 DESCRIPTION
+
+A regular text box.
+
+This control implements L<FormFactory::Control>, L<FormFactory::Control::Role::Labeled>, and L<Formfactory::Control::Role::ScalarValue>.
+
+=head1 ATTRIBUTES
+
+=head2 value
+
+The current value of the control.
+
+=cut
+
+
 has value => (
     is        => 'rw',
     isa       => 'Str',
     predicate => 'has_value',
 );
+
+=head2 default_value
+
+The default value of the control.
+
+=cut
 
 has default_value => (
     is        => 'rw',
@@ -19,9 +54,23 @@ has default_value => (
     predicate => 'has_default_value',
 );
 
+=head2 stashable_keys
+
+The L</value> is stashed.
+
+=cut
+
 has '+stashable_keys' => (
     default   => sub { [ qw( value ) ] },
 );
+
+=head1 METHODS
+
+=head2 current_value
+
+If the L</value> is set, use that. If the L</default_value> is set use that. Otherwise, return the empty string.
+
+=cut
 
 sub current_value {
     my $self = shift;
@@ -30,5 +79,18 @@ sub current_value {
          : $self->has_default_value ? $self->default_value
          :                            '';
 }
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp C<< <hanenkamp@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Qubling Software LLC.
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 1;
