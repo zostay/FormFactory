511e654c63fc4d30bfc1e02ae8fdc3c65145db14
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-11-25 17:05:03 -0600

    Adding POD to FormFactory::Feature::Control::Trim

--- lib/FormFactory/Feature/Control/Trim.pm
+++ lib/FormFactory/Feature/Control/Trim.pm
@@ -6,6 +6,31 @@ with qw(
     FormFactory::Feature::Role::Control
 );
 
+=head1 NAME
+
+FormFactory::Feature::Control::Trim - Trims whitespace from a control value
+
+=head1 SYNOPSIS
+
+  has_control title => (
+      control => 'text',
+      features => {
+          trim => 1,
+      },
+  );
+
+=head1 DESCRIPTION
+
+Strips whitespace from the front and back of the given values.
+
+=head1 METHODS
+
+=head2 check_control
+
+Reports an error unless the control is a scalar value.
+
+=cut
+
 sub check_control {
     my ($self, $control) = @_;
 
@@ -13,6 +38,12 @@ sub check_control {
 
     die "the trim feature only works on scalar values, not $control";
 }
+
+=head2 clean_value
+
+Strips whitespace from the start and end of the control value.
+
+=cut
 
 sub clean_value {
     my $self    = shift;
@@ -24,5 +55,18 @@ sub clean_value {
 
     $control->current_value($value);
 }
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp C<< <hanenkamp@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Qubling Software LLC.
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 1;
