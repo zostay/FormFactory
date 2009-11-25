08b94f0d531ada692b1bd72599b412f992ca8a30
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-11-25 16:58:45 -0600

    Adding POD to FormFactory::Feature::Control::Required

--- lib/FormFactory/Feature/Control/Required.pm
+++ lib/FormFactory/Feature/Control/Required.pm
@@ -6,6 +6,31 @@ with qw(
     FormFactory::Feature::Role::Control
 );
 
+=head1 NAME
+
+FormFactory::Feature::Control::Required - Makes sure a value is set on a control
+
+=head1 SYNOPSIS
+
+  has_control last_name => (
+      control => 'text',
+      features => {
+          required => 1,
+      },
+  );
+
+=head1 DESCRIPTION
+
+Reports a check error if the required value is not set. On scalar value controls, it checks that the value has a length greater than zero. On list value controls, it makes sure the list of selected items has more than zero elements.
+
+=head1 METHODS
+
+=head2 check_control
+
+Only works with scalar and list valued controls.
+
+=cut
+
 sub check_control {
     my ($self, $control) = @_;
 
@@ -14,6 +39,12 @@ sub check_control {
 
     die "the required feature does not know how to check the value of $control";
 }
+
+=head2 check_value
+
+Reports an error if a scalar value does not have a length greater than 0. Reports an error if a list value has 0 items in the list.
+
+=cut
 
 sub check_value {
     my $self    = shift;
@@ -35,5 +66,18 @@ sub check_value {
         }
     }
 }
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp C<< <hanenkamp@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Qubling Software LLC.
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 1;
