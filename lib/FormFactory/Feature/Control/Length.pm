f0c25de57f9081776a81d6d806f3b6190f0b9fd1
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-11-25 15:30:24 -0600

    Adding POD to FormFactory::Feature::Control::Length

--- lib/FormFactory/Feature/Control/Length.pm
+++ lib/FormFactory/Feature/Control/Length.pm
@@ -6,11 +6,45 @@ with qw(
     FormFactory::Feature::Role::Control
 );
 
+=head1 NAME
+
+FormFactory::Feature::Control::Length - A control feature for checking length
+
+=head1 SYNOPSIS
+
+  has_control login_name => (
+      control => 'text',
+      feature => {
+          length => {
+              minimum => 3,
+              maximum => 15,
+          },
+      },
+  );
+
+=head1 DESCRIPTION
+
+Linked to a control, it checks to see that the string is not too short or too long.
+
+=head1 ATTRIBUTES
+
+=head2 minumum
+
+The optional minimum length permitted for the string.
+
+=cut
+
 has minimum => (
     is        => 'ro',
     isa       => 'Int',
     predicate => 'has_minimum',
 );
+
+=head2 maximum
+
+The optional maximum length permitted for the string.
+
+=cut
 
 has maximum => (
     is        => 'ro',
@@ -30,6 +64,14 @@ sub BUILDARGS {
     return $class->SUPER::BUILDARGS(@_);
 }
 
+=head1 METHODS
+
+=head2 check_control
+
+Makes sure the value is a L<FormFactory::Control::Role::ScalarValue>.
+
+=cut
+
 sub check_control {
     my ($self, $control) = @_;
 
@@ -37,6 +79,12 @@ sub check_control {
 
     die "the length feature only works with scalar values\n";
 }
+
+=head2 check_value
+
+Verifies that the value of the control is not too short or too long.
+
+=cut
 
 sub check_value {
     my $self  = shift;
@@ -56,5 +104,18 @@ sub check_value {
         );
     }
 }
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp C<< <hanenkamp@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Qubling Software LLC.
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 1;
