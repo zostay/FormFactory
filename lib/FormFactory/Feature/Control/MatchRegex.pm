f10d3257833a8b6c2d8b0371ff4e8d90841f9d76
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-11-25 16:22:11 -0600

    Add POD to FormFactory::Feature::Control::MatchRegex

--- lib/FormFactory/Feature/Control/MatchRegex.pm
+++ lib/FormFactory/Feature/Control/MatchRegex.pm
@@ -6,11 +6,47 @@ with qw(
     FormFactory::Feature::Role::Control
 );
 
+=head1 NAME
+
+FormFactory::Feature::Control::MatchRegex - Match a control value against a regex
+
+=head1 SYNOPSIS
+
+  has_control five_char_palindrome => (
+      control => 'text',
+      features => {
+          match_regex => {
+              regex => qr/(.)(.).\2\1/,
+              message => 'the %s is not a palindrome',
+          },
+      },
+  );
+
+=head1 DESCRIPTION
+
+Checks that the control value matches a regular expression. Returns an error if it does not.
+
+=head1 ATTRIBUTES
+
+=head2 regex
+
+The regular expression to use.
+
+=cut
+
 has regex => (
     is        => 'ro',
     isa       => 'Regexp',
     required  => 1,
 );
+
+=head1 METHODS
+
+=head2 check_control
+
+Checks that the control does L<FormFactory::Control::Role::ScalarValue>.
+
+=cut
 
 sub check_control {
     my ($self, $control) = @_;
@@ -19,6 +55,12 @@ sub check_control {
 
     die "the match_regex feature only works with scalar value controls, not $control";
 }
+
+=head2 check_value
+
+Runs the regular expression against the current value of the control and reports an error if it does not match.
+
+=cut
 
 sub check_value {
     my $self  = shift;
@@ -29,5 +71,18 @@ sub check_value {
         $self->control_error("the %s does not match $regex");
     }
 }
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp C<< <hanenkamp@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Qubling Software LLC.
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 1;
