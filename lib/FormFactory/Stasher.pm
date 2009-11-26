a0ee934ce074ab32d74b9a8ea85eb5820d2bcbc1
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-11-26 07:57:42 -0600

    Add POD to FormFactory::Stasher and FormFactory::Stasher::Memory

--- lib/FormFactory/Stasher.pm
+++ lib/FormFactory/Stasher.pm
@@ -3,4 +3,43 @@ use Moose::Role;
 
 requires qw( stash unstash );
 
+=head1 NAME
+
+FormFactory::Stasher - An object responsible for remembering things
+
+=head1 DESCRIPTION
+
+A stasher remembers things.
+
+=head1 ROLE METHODS
+
+=head2 stash
+
+  $stasher->stash($key, $hashref);
+
+Given a C<$key> to store it under and a C<$hashref> to store. Remember the given information for recall with L</unstash>.
+
+=head2 unstash
+
+  my $hashref = $stasher->unstash($key);
+
+Given a C<$key>, recall a previously stored C<$hashref>.
+
+=head1 SEE ALSO
+
+L<FormFactory::Stasher::Memory>
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp C<< <hanenkamp@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Qubling Software LLC.
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
 1;
--- lib/FormFactory/Stasher/Memory.pm
+++ lib/FormFactory/Stasher/Memory.pm
@@ -3,6 +3,32 @@ use Moose;
 
 with qw( FormFactory::Stasher );
 
+=head1 NAME
+
+FormFactory::Stasher::Memory - Remember things in a Perl hash
+
+=head1 SYNOPSIS
+
+  $c->session->{stash_stuff} ||= {};
+  my $stasher = FormFactory::Stasher::Memory->new(
+      stash_hash => $c->session->{stash_stuff},
+  );
+
+  $stasher->stash(foo => { blah => 1 });
+  my $bar = $stasher->unstash('bar');
+
+=head1 DESCRIPTION
+
+Stashes things into a plain memory hash. This is useful if you already have a mechanism for remember things that can be reused via a hash.
+
+=head1 ATTRIBUTES
+
+=head2 stash_hash
+
+The hash reference to stash stuff into. Defaults to an empty anonymous hash.
+
+=cut
+
 has stash_hash => (
     is        => 'rw',
     isa       => 'HashRef',
@@ -10,14 +36,41 @@ has stash_hash => (
     default   => sub { {} },
 );
 
+=head1 METHODS
+
+=head2 stash
+
+Stash the stuff given.
+
+=cut
+
 sub stash {
     my ($self, $moniker, $stash) = @_;
     $self->stash_hash->{ $moniker } = $stash;
 }
 
+=head2 unstash
+
+Unstash the stuff requested.
+
+=cut
+
 sub unstash {
     my ($self, $moniker) = @_;
     delete $self->stash_hash->{ $moniker };
 }
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp C<< <hanenkamp@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Qubling Software LLC.
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 1;
