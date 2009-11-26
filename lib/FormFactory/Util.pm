450790029730b36c2893c8e602846c6ac4a38c2a
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-11-26 08:00:10 -0600

    Adding POD to FormFactory::Util

--- lib/FormFactory/Util.pm
+++ lib/FormFactory/Util.pm
@@ -7,11 +7,42 @@ use Sub::Exporter -setup => {
     ) ],
 };
 
+=head1 NAME
+
+FormFactory::Util - Utility subroutines that don't belong anywhere else
+
+=head1 DESCRIPTION
+
+Utility subroutines that don't belong anywhere else.
+
+=head1 METHODS
+
+=head2 class_name_from_name
+
+  my $class_name = class_name_from_name($name);
+
+Used to build capitalized class names from all lowercase names.
+
+=cut
+
 sub class_name_from_name($) {
     my ($name) = @_;
 
     $name =~ s/(?:[^A-Za-z]+|^)([A-Za-z])/\U$1/g;
     return ucfirst $name;
 }
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp C<< <hanenkamp@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Qubling Software LLC.
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 1;
