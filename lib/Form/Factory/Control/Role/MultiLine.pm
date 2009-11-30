4929de0b57248e3be0b0363879efe64015116269
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-11-30 14:26:25 -0600

    Adding a MultiLine role

---
+++ lib/Form/Factory/Control/Role/MultiLine.pm
@@ -0,0 +1,25 @@
+package Form::Factory::Control::Role::MultiLine;
+use Moose::Role;
+
+=head1 NAME
+
+Form::Factory::Control::Role::MultiLine - multiline controls
+
+=head1 DESCRIPTION
+
+Marks controls that have a multiline text entry.
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp C<< <hanenkamp@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Qubling Software LLC.
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
