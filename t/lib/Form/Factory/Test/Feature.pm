cc4eddf3c850334fa187dbd331cc870c3796bf80
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-12-08 20:21:18 -0600

    Fixing the test for the new feature roles that are coming

--- t/lib/Form/Factory/Test/Feature.pm
+++ t/lib/Form/Factory/Test/Feature.pm
@@ -28,12 +28,39 @@ has feature => (
     required   => 1,
 );
 
-test plan => 2, basic_feature_checks => sub {
+test plan => 5, basic_feature_checks => sub {
     my $self = shift;
     my $feature = $self->feature;
 
     does_ok($feature, 'Form::Factory::Feature');
-    can_ok($feature, qw( clean check pre_process post_process ));
+
+    if ($feature->does('Form::Factory::Feature::Role::Clean')) {
+        can_ok($feature, qw( clean ));
+    }
+    else {
+        pass('not a cleaner');
+    }
+
+    if ($feature->does('Form::Factory::Feature::Role::Check')) {
+        can_ok($feature, qw( check ));
+    }
+    else {
+        pass('not a checker');
+    }
+
+    if ($feature->does('Form::Factory::Feature::Role::PreProcess')) {
+        can_ok($feature, qw( pre_process ));
+    }
+    else {
+        pass('not a pre_processor');
+    }
+
+    if ($feature->does('Form::Factory::Feature::Role::PostProcess')) {
+        can_ok($feature, qw( post_process ));
+    }
+    else {
+        pass('not a post_processor');
+    }
 };
 
 1;
