6862fa0289a09ccbfc36e055eddcf7895766d5cd
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-12-08 22:41:39 -0600

    Clean up TO DO notes about controls.

--- lib/Form/Factory.pm
+++ lib/Form/Factory.pm
@@ -124,7 +124,7 @@ This is not definite, but some things I know as of right now I'm not happy with:
 
 =item *
 
-There are lots of tweaks coming to controls, especially L<Form::Factory::Control::Button>. I'm not very happy with how this is done right now, so something will change.
+There are lots of tweaks coming to controls.
 
 =item *
 
