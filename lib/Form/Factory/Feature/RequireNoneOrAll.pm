abd63e45c3d6a5b7585be5008f45214ff63cf2fe
Sterling Hanenkamp <hanenkamp@cpan.org>
2010-01-02 20:57:39 -0600

    Fixing missing LICENSE POD

--- lib/Form/Factory/Feature/RequireNoneOrAll.pm
+++ lib/Form/Factory/Feature/RequireNoneOrAll.pm
@@ -125,4 +125,17 @@ sub _control_label {
     return $control_label;
 }
 
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp C<< <hanenkamp@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Qubling Software LLC.
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
 1;
